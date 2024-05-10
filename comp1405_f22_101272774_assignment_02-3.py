9928a545010118e215f35a05848cdc028185d9d1
hiradshowghi <118714159+hiradshowghi@users.noreply.github.com>
2024-05-10 19:49:41 -0400

    Add files via upload

---
+++ comp1405_f22_101272774_assignment_02-3.py
@@ -0,0 +1,33 @@
+#Hirad Showghi Sarkhosh
+#101272774
+
+import sys
+
+
+x=int(sys.argv[1])
+
+y=int(input("Enter a Number"))
+
+z=y%3
+print(z)
+
+a=z+(z+1)
+print(a)
+
+b=a**3
+print(b)
+
+c=b+x
+print(c)
+
+d=c*0.496403
+print(d)
+
+e=int(d)
+
+f=chr(e)
+print("<",e,">")
+
+print(f)
+
+
